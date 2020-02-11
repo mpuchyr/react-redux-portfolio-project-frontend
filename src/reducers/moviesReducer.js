602958bca67490e6ea887c8aa3925f865559ec98
mpuchyr <matt.puchyr@gmail.com>
2020-02-11 15:43:40 -0600

    Create combine reducer in reducers/index.js

--- src/reducers/moviesReducer.js
+++ src/reducers/moviesReducer.js
@@ -1,5 +1,9 @@
 export default(state = [], action) => {
     switch(action.type) {
+        case 'LOADING_MOVIES':
+            return {
+                
+            }
         default:
             return state
     }
