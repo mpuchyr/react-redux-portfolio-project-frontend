de3c51914500ab5a286f7dbc95f15cc32af46e5b
mpuchyr <matt.puchyr@gmail.com>
2020-02-11 15:54:21 -0600

    Add composeEnhancers, store, and Provider to index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,16 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+let store = createStore(rootReducer, composeEnhancers())
+
+ReactDOM.render(
+    
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
