33057fc3503afa690aad68377bc03e75c90263b8
mpuchyr <matt.puchyr@gmail.com>
2020-02-11 15:51:36 -0600

    Create various necessary imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, compose } from 'redux'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './reducers'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
