e8dbb7076e556ffa2ee47a1c4ccfa4bda8e5f30a
mpuchyr <matt.puchyr@gmail.com>
2020-02-11 18:54:46 -0600

    Configure containers/MovieShow to find and show a specific movie based on the params id

--- src/containers/MovieShow.js
+++ src/containers/MovieShow.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
 
 class MovieShow extends Component {
+
+    showMovie = () => {
+        if (this.props.movies.length > 0) {
+            let paramsId = parseInt(this.props.match.params.id, 10)
+            let movie = this.props.movies.find(movie => movie.id === paramsId)
+            if (movie) {
+                return (
+                    <div>
+                        <img src={movie.poster_url} alt={movie.title}></img>
+                        <h1>{movie.title}</h1>
+                        <p>{movie.synopsis}</p>
+                    </div>
+
+                )
+            } else {
+                return <h1>Movie Not Found</h1>
+            }
+        } else {
+            return <p>Loading..</p>
+        }
+    }
+
     render() {
-        return (
+        
+
+        return (   
             <div>
-                Connected
+                {this.showMovie()}
             </div>
         )
     }
