432addea7e8f856ffd94112c1a689c26a9013c93
mpuchyr <matt.puchyr@gmail.com>
2020-02-12 15:26:22 -0600

    Add necessary imports to containers/AllMovieShow.js

--- src/containers/AllMovieShow.js
+++ src/containers/AllMovieShow.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { fetchMovies } from '../actions'
 
 class AllMovieShow extends Component {
 
@@ -9,4 +12,17 @@ class AllMovieShow extends Component {
     }
 }
 
-export default AllMovieShow
+const mapStateToProps = state => {
+    return {
+      movies: state.movieState.movies,
+      loading: state.movieState.loading
+    }
+  }
+
+const mapDispatchToProps = dispatch => {
+    return {
+      fetchMovies: movies => dispatch(fetchMovies(movies))
+    }
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllMovieShow)
