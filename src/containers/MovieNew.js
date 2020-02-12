e44632c99d7e4e6307a99454d1ded91b1cd38ffc
mpuchyr <matt.puchyr@gmail.com>
2020-02-11 19:55:05 -0600

    Create handleOnChange function in containers/MovieNew.js

--- src/containers/MovieNew.js
+++ src/containers/MovieNew.js
@@ -13,13 +13,19 @@ class MovieNew extends Component {
         }
     }
 
+    handleOnChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     render() {
         return (
             <form>
                 <h3>Add a Movie</h3>
-                <input type="text" name="title" id="title" value={this.state.title}/>
-                <input type="textarea" name="synopsis" id="synopsis" value={this.state.synopsis} />
-                <input type="text" name="poster_url" id="poster_url" value={this.state.poster_url} />
+                <input type="text" name="title" id="title" value={this.state.title} onChange={(event) => this.handleOnChange(event)}/>
+                <input type="textarea" name="synopsis" id="synopsis" value={this.state.synopsis} onChange={(event) => this.handleOnChange(event)}/>
+                <input type="text" name="poster_url" id="poster_url" value={this.state.poster_url} onChange={(event) => this.handleOnChange(event)}/>
                 <input type="submit">Add Movie</input>
             </form>
         )
