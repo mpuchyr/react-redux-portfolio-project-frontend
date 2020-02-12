db859674d15bfbca5bb4369d500d4a7781b45db1
mpuchyr <matt.puchyr@gmail.com>
2020-02-11 19:52:35 -0600

    Create form in containers/MovieNew.js

--- src/containers/MovieNew.js
+++ src/containers/MovieNew.js
@@ -15,8 +15,13 @@ class MovieNew extends Component {
 
     render() {
         return (
-            <div>
-            </div>
+            <form>
+                <h3>Add a Movie</h3>
+                <input type="text" name="title" id="title" value={this.state.title}/>
+                <input type="textarea" name="synopsis" id="synopsis" value={this.state.synopsis} />
+                <input type="text" name="poster_url" id="poster_url" value={this.state.poster_url} />
+                <input type="submit">Add Movie</input>
+            </form>
         )
     }
 }
