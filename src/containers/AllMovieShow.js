66516b6eaa0cf771741a81141d0c0de61a6c162a
mpuchyr <matt.puchyr@gmail.com>
2020-02-12 15:23:48 -0600

    Create AllMovieShow.js in containers

---
+++ src/containers/AllMovieShow.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class AllMovieShow extends Component {
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default AllMovieShow
