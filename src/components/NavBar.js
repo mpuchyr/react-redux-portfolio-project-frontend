6976e454bb68b199ce6c9f51ed96db21fc031d44
mpuchyr <matt.puchyr@gmail.com>
2020-02-11 19:14:24 -0600

    Create basic structure for components/NavBar.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+
+class NavBar extends Component {
+    render() {
+        <div>
+        </div>
+    }
+}
+
+export default NavBar
