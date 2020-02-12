075d6e13185a14c8c58a3e9598351cf2f39b7b79
mpuchyr <matt.puchyr@gmail.com>
2020-02-11 19:26:39 -0600

    Create addMovie in actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,3 +8,19 @@ export const fetchMovies = () => {
         })
     }
 }
+
+export const addMovie = (movie) => {
+    return (dispatch) => {
+        return fetch('http://localhost:3000/movies', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringifty({movie: movie})
+        })
+        .then(res => res.json())
+        .then(movie => {
+            dispatch({type: "ADD_MOVIE", payload: movie})
+        })
+    }
+}
