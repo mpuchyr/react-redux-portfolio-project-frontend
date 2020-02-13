b5bdef7202bc5b15bc3287a34868e026b3a53271
mpuchyr <matt.puchyr@gmail.com>
2020-02-12 19:03:57 -0600

    Choose 9 random movie posters to display in renderMoviePosters in containers/Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -9,13 +9,24 @@ class Home extends Component {
         this.props.fetchMovies()
       }
     
+
     renderMoviePosters = () => {
+        
+        let movies = []
         if (this.props.loading) {
             return <p>Loading...</p>
         } else {
-            
-            return this.props.movies.map(movie => {
-                console.log(movie)
+            if (this.props.movies.length > 0) {
+              while (movies.length < 9) {
+                let randNum = Math.floor(Math.random() * this.props.movies.length)
+                let movie = this.props.movies[randNum]
+                if (!movies.includes(movie)) {
+                  movies.push(movie)
+                }
+              }
+            }
+
+            return movies.map(movie => {
                 const link = `/movies/${movie.id}`
                 return <NavLink to={link} key={movie.id}><img className="poster" src={movie.poster_url} key={movie.id} alt={movie.title}></img></NavLink>
             })
@@ -26,6 +37,7 @@ class Home extends Component {
         return(
             <div>
                 <h1>Movie Archive</h1>
+                <h2>9 Movie Picks For You</h2>
                 {this.renderMoviePosters()}
             </div>
         )
