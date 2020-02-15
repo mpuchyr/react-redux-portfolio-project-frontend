bb99e99391fcba1203095f8c17d3b8f1841fe553
mpuchyr <matt.puchyr@gmail.com>
2020-02-15 12:04:25 -0600

    Create createGenreLinks function in components/GenreNavBar.js

--- src/components/GenreNavBar.js
+++ src/components/GenreNavBar.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react'
 
 class GenreNavBar extends Component {
+    
+    createGenreLinks = () => {
+        let genres = this.props.genres.sort( (a, b) => {
+            if (a.name < b.name) {
+                return -1
+            } else if (a.name > b.name) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+        return genres.map(genre => {
+            let genreLink = `#${genre.name}` 
+            return (
+                <>
+                    <a href={genreLink}><button>{genre.name}</button></a>
+                
+                    {this.showMoviesInGenre(genre.id)}
+                </>
+            )
+                
+                
+        })
+    }
+    
     render() {
         return(
             <div className="genre-nav-bar">
+                {this.createGenreLinks()}
             </div>
         )
     }
