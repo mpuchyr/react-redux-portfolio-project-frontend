305ee928dbe1c712550af12f00bee07341d44e8d
mpuchyr <matt.puchyr@gmail.com>
2020-02-16 17:08:53 -0600

    Create findMovie in containers/MovieEdit.js to find a specific movie

--- src/containers/MovieEdit.js
+++ src/containers/MovieEdit.js
@@ -7,12 +7,28 @@ class MovieEdit extends Component {
 
 
 
+    findMovie = () => {
+        if (this.props.movies.length > 0) {
+            let paramsId = parseInt(this.props.match.params.id, 10)
+            let movie = this.props.movies.find(movie => movie.id === paramsId)
+            if (movie) {
+                this.setState({
+                    id: movie.id,
+                    title: movie.title,
+                    synopsis: movie.synopsis,
+                    poster_url: movie.poster_url,
+                    genre_id: movie.genre_id
+                })
+            }
+        }
+    }
+
+
 
 
     render() {
         return (
             <div>
-
             </div>
         )
     }
