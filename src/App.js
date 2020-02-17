2f162436a9a7171a3d9f9944406c8795cb94bb2a
mpuchyr <matt.puchyr@gmail.com>
2020-02-17 14:41:10 -0600

    Remove unused code from App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { connect } from 'react-redux'
 import NavBar from './components/NavBar'
 import Home from './containers/Home'
 import MovieShow from './containers/MovieShow'
 import MovieNew from './containers/MovieNew'
 import AllMovieShow from './containers/AllMovieShow'
 import MovieEdit from './containers/MovieEdit'
-import { fetchMovies } from './actions'
+
 class App extends Component {
-
-  // componentDidMount() {
-  //   this.props.fetchMovies()
-  // }
-
 
 
   render() {
@@ -37,22 +31,5 @@ class App extends Component {
   }
 }
 
-
-
-// const mapStateToProps = state => {
-//   return {
-//     movies: state.movieState.movies,
-//     loading: state.movieState.loading
-//   }
-// }
-
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     fetchMovies: movies => dispatch(fetchMovies(movies))
-//   }
-// }
-
-
-// export default connect(mapStateToProps, mapDispatchToProps)(App)
 export default App
 
